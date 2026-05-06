0272d775c33a1bdd40018608a2e63d1c289a13c3
luubinhan <luubinhan1989@gmail.com>
2026-05-06 13:03:50 +0700

    Add PlaceToLive to SPEAKING_LEVELS for enhanced speaking practice

--- data/speaking-tourism.ts
+++ data/speaking-tourism.ts
@@ -2,9 +2,11 @@ import { Level } from '../types';
 import { Tourism } from './speaking/tourism';
 import { Family } from './speaking/family';
 import { LearnMost } from './speaking/most';
+import { PlaceToLive } from './speaking/place-to-live';
 
 export const SPEAKING_LEVELS: Level[] = [
   Tourism,
   Family,
   LearnMost,
+  PlaceToLive,
 ];
