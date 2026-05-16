3ee8a976f8321b480fc510aa4767f8307f7bb97f
luubinhan <luubinhan1989@gmail.com>
2026-05-16 13:54:12 +0700

    Add FlexibleRoutine level to enhance speaking practice with diverse scenarios

--- data/speaking-tourism.ts
+++ data/speaking-tourism.ts
@@ -8,8 +8,10 @@ import { Risk } from './speaking/risk';
 import { Introduce } from './speaking/introduce';
 import { WorkingInTheOffice } from './speaking/working-in-the-office';
 import { ProudProject } from './speaking/proud-project';
+import { FlexibleRoutine } from './speaking/flexiable-routine';
 
 export const SPEAKING_LEVELS: Level[] = [
+  FlexibleRoutine,
   ProudProject,
   Risk,
   Introduce,
