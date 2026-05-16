9b2d9cc6d3649e5be21897aff760b838a1520483
luubinhan <luubinhan1989@gmail.com>
2026-05-16 13:48:11 +0700

    Add ProudProject level to enhance speaking practice with personal storytelling

--- data/speaking-tourism.ts
+++ data/speaking-tourism.ts
@@ -7,8 +7,10 @@ import { TimeOfTheYear } from './speaking/time-of-the-year';
 import { Risk } from './speaking/risk';
 import { Introduce } from './speaking/introduce';
 import { WorkingInTheOffice } from './speaking/working-in-the-office';
+import { ProudProject } from './speaking/proud-project';
 
 export const SPEAKING_LEVELS: Level[] = [
+  ProudProject,
   Risk,
   Introduce,
   Tourism,
